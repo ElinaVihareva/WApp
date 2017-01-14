7fd7cb4e52b7267ce0a6f51eab1f569c3b18b802
vihareva <vihareva@N0039>
2017-01-14 19:20:03 +0400

    sdfsd

--- CodeFirst/Model.cs
+++ CodeFirst/Model.cs
@@ -11,7 +11,7 @@ namespace CodeFirst
     {
         public class WorkflowInstance
         {
-          
+
             public int Id { get; set; }
             public Guid uid { get; set; }
             public long process { get; set; }
@@ -22,62 +22,61 @@ namespace CodeFirst
             public Guid elementUid { get; set; }
             public long elmaId { get; set; }
 
+        }
+        public class WorkflowBookmark
+        {
 
-            public class WorkflowBookmark
-            {
+            public int Id { get; set; }
+            public Guid uid { get; set; }
+            public Guid elementUid { get; set; }
+            public long instance { get; set; }
+            public long elmaId { get; set; }
+        }
 
-                public  int Id { get; set; }
-                public Guid uid { get; set; }
-                public Guid elementUid { get; set; }
-                public long instance { get; set; }
-                public long elmaId { get; set; }
-            }
+        public class TaskBase
+        {
 
-            public class TaskBase
-            {
+            public int Id { get; set; }
+            public long executor { get; set; }
+            public string subject { get; set; }
+            public DateTime startDate { get; set; }
+            public DateTime endWorkDate { get; set; }
+            public long workflowBookmark { get; set; }
+            public long elmaId { get; set; }
+        }
 
-                public  int Id { get; set; }
-                public long executor { get; set; }
-                public string subject { get; set; }
-                public DateTime startDate { get; set; }
-                public DateTime endWorkDate { get; set; }
-                public long workflowBookmark { get; set; }
-                public long elmaId { get; set; }
-            }
+        public class WorkflowTrackingItem
+        {
 
-            public class WorkflowTrackingItem
-            {
-          
-                public int Id { get; set; }
-                Guid elementUid { get; set; }
-                DateTime startDate { get; set; }
-                DateTime endDate { get; set; }
-                long elmaId { get; set; }
-            }
+            public int Id { get; set; }
+            Guid elementUid { get; set; }
+            DateTime startDate { get; set; }
+            DateTime endDate { get; set; }
+            long elmaId { get; set; }
+        }
 
-            public class WorkflowProcess
-            {
-              
-                public int Id { get; set; }
-                string name { get; set; }
-                long header { get; set; }
-                long versionNumber { get; set; }
-                long elmaId { get; set; }
-            }
+        public class WorkflowProcess
+        {
 
-            public class ELMAConnetion
-            {
-                string address { get; set; }
-                string login { get; set; }
-                string password { get; set; }
-                public int Id { get; set; }
-            }
+            public int Id { get; set; }
+            string name { get; set; }
+            long header { get; set; }
+            long versionNumber { get; set; }
+            long elmaId { get; set; }
+        }
 
-            public class ProcessHeader
-            {
-                public int Id { get; set; }
-                public string Name { get; set; }
-            }
+        public class ELMAConnetion
+        {
+            string address { get; set; }
+            string login { get; set; }
+            string password { get; set; }
+            public int Id { get; set; }
+        }
+
+        public class ProcessHeader
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
     }
 }
